e7e8ca3fbf4384867ef8babdd46679bb5c8bf516
MajorGeneralCommand <oliver.dehnardt21@gmail.com>
2023-12-08 22:28:19 +0100

    Added testing checks, hidden token, repo public

--- main/index.js
+++ main/index.js
@@ -11,4 +11,16 @@ const client = new Client({
 })
 
 
+client.on("ready", (c) => {
+    console.log(`Client ready for use! ${c.user.username} version: Unknown`)
+})
+
+client.on("messageCreate", (message) => {
+    console.log(`User: ${message.author.username}, Content: ${message.content}`)
+
+    if(message.content == "kosbot-replytest") {
+        message.reply(`Replying to ${message.author.username}`)
+    }
+})
+
 client.login("MTE4Mjc4MDc2OTA0Mzc1MDk5Mg.Gn3-Ep.dEzV_NrWfY4fp4j1emozDapcTt5-hAmB1pTLu0");
